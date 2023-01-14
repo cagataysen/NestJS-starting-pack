421dc6854e698adbf7d0da6425c5314563889b87
[cagataysen] <drosen_26@hotmail.com>
2023-01-15 02:00:10 +0300

    Prisma modules integrated into code

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,7 +6,8 @@ export class AuthController {
     constructor(private authService: AuthService) {}
     
     @Post('signup')
-    signup(){
+    signup(@Req() req: Request){
+        console.log(req.body);
         return this.authService.signup();
     }
     @Post('signin')
